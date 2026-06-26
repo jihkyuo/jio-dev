90b0ea636e86d2777d984807b75fed675b86d6d0
지오현 <jihkyuoo@naver.com>
2026-06-26 11:00:00 +0900

    ✨ feat: 콘텐츠 API 배럴 추가

---
+++ src/content/index.ts
@@ -0,0 +1,10 @@
+export { getProfile } from "@/content/profile";
+export { getExperience } from "@/content/experience";
+export { getSkills } from "@/content/skills";
+export { getProjects, getProjectSlugs } from "@/content/projects";
+export type {
+  Profile,
+  Experience,
+  Skills,
+  ProjectMeta,
+} from "@/content/schema";
